5cae0b09bfa5e98a6c9f7cf9c509c7d273ce35e2
Divyansh Garg <garg_divyansh@icloud.com>
2025-11-04 14:36:07 +0800

    Add files via upload

---
+++ Amazing-Race/locomotion.ino.ino
@@ -0,0 +1,97 @@
+#include "MeMCore.h"
+
+#define TIMEOUT 2000
+#define SPEED_OF_SOUND 340
+#define ULTRASONIC 12
+
+long dist();
+void forward(int pid) {
+  // ✅ Fixed: Inverted correction so robot turns AWAY from wall
+  if (pid < 0) {
+     leftMotor.run(motorSpeed - pid);
+    rightMotor.run(-motorSpeed - pid);
+  } else {
+    leftMotor.run(motorSpeed + pid);
+    rightMotor.run(-motorSpeed + pid);
+  }
+
+
+  Serial.print("PID: "); Serial.print(pid);
+  Serial.print(" | L: "); Serial.print(motorSpeed + pid);
+  Serial.print(" | R: "); Serial.print(-motorSpeed - pid);
+  Serial.print(" | Dist: "); Serial.println(dist());
+}
+
+double kp, ki, kd;
+double errsum, lasterr;
+unsigned long lsttime;
+
+void initialize() {
+  kp = 4.0;
+  ki = 0.0;
+  kd = 2; 
+  lsttime = millis();
+}
+
+int calcpid() {
+  unsigned long now = millis();
+  double dt = (now - lsttime) / 1000.0;
+  if (dt <= 0) dt = 0.001;
+
+  long setpoint = 11;
+  double distance = dist();
+  double error = setpoint - distance;
+
+  errsum += error * dt;
+  double dErr = (error - lasterr) / dt;
+  double pid = kp * error + ki * errsum + kd * dErr;
+
+  if (pid > 255) pid = 255;
+  if (pid < -255) pid = -255;
+
+  lasterr = error;
+  lsttime = now;
+
+  // return 0;
+  return pid;
+}
+
+void go_forward(){
+  double pid = calcpid();
+  forward(pid);
+  delayMicroseconds(50);
+}
+
+void turn_left(){
+  leftMotor.run(motorSpeed);
+  rightMotor.run(motorSpeed);
+  delay(400);
+}
+
+void turn_right(){
+  leftMotor.run(-motorSpeed);
+  rightMotor.run(-motorSpeed);
+  delay(400);
+}
+void turn_U(){
+  leftMotor.run(motorSpeed);
+  rightMotor.run(motorSpeed);
+  delay(800);
+}
+void successive_left(){
+  turn_left();
+  for(int i=0;i<10;++i){
+    go_forward();
+  }
+  turn_left();
+}
+void successive_right(){
+  turn_right();
+  go_forward();
+  delay(330);
+  turn_right();
+}
+void stop(){
+  leftMotor.run(0);
+  rightMotor.run(0);
+}
