c4d4092d701385f36ff103010f6f3187bbe416cb
[vulturechoujin] <E1528675@u.nus.edu>
2025-11-07 01:24:47 +0800

    Local Change

--- Amazing-Race/SensorFunctions.ino
+++ Amazing-Race/SensorFunctions.ino
@@ -4,7 +4,7 @@
 #define IR_EMITTER_PIN 14
 #define IR_RECIEVER_PIN 16
 
-long ultrasonic_distance() {
+float ultrasonic_distance() {
     pinMode(ULTRASONIC_PIN, OUTPUT);
 
     digitalWrite(ULTRASONIC_PIN, LOW);
@@ -19,8 +19,8 @@ long ultrasonic_distance() {
 }
 
 long ir_distance() {
-    pinMode(IR_EMITTER_PIN, INPUT);
-    pinMode(IR_RECIEVER_PIN, OUTPUT);
+    pinMode(IR_EMITTER_PIN, OUTPUT);
+    pinMode(IR_RECIEVER_PIN, INPUT);
     digitalWrite(IR_EMITTER_PIN, HIGH);
     delayMicroseconds(500);
 
@@ -29,7 +29,7 @@ long ir_distance() {
         total += analogRead(IR_RECIEVER_PIN);
         delayMicroseconds(10);
     }
-    analogWrite(IR_EMITTER_PIN, LOW);
+    digitalWrite(IR_EMITTER_PIN, LOW);
     double volt = 5.0 * (total / 10.0) / 1023.0;
     float distance = 27.86 * pow(volt, -1.15) - 3.0;
     distance = 2.0 * distance - 2;
