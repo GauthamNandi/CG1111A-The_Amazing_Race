66d14fff8aa226a02901b83dc03cc637181e5dc7
Gautham <124564178+GauthamNandi@users.noreply.github.com>
2025-11-10 21:08:51 +0800

    Update SensorFunctions.ino

--- Amazing-Race/SensorFunctions.ino
+++ Amazing-Race/SensorFunctions.ino
@@ -1,7 +1,7 @@
 #define TIMEOUT 5000     
 #define SPEED_OF_SOUND 340 
 #define ULTRASONIC_PIN 12
-// #define IR_EMITTER_PIN 14
+#define IR_EMITTER_PIN 14
 #define IR_RECIEVER_PIN 16
 
 float ultrasonic_distance() {
@@ -31,17 +31,23 @@ long ir_distance() {
     }
     
     input(0); // digitalWrite(IR_EMITTER_PIN, LOW);
-    double volt = 5.0 * (total / 10.0) / 1023.0;
-    float distance = 27.86 * pow(volt, -1.15) - 3.0;
-    distance = 2.0 * distance - 2;
+    // double volt = 5.0 * (total / 10.0) / 1023.0;
+    // float distance = 27.86 * pow(volt, -1.15) - 3.0;
+    // distance = 2.0 * distance - 2;
+    float adc = total / 10.0;
+    float distance = 270.0 * pow(adc, -1.1);
+    if (distance < 1) distance = 1;
+    if (distance > 20) distance = 20;
     return distance;
 }
 
 long find_distance() {
-    Serial.println(ir_distance());
-    long u_dist = ultrasonic_distance();
-    if (u_dist == 0 || u_dist > 20) {
-        return ir_distance();
-    }
+    // Serial.println(ir_distance());
+    // long u_dist = ultrasonic_distance();
+    // if (u_dist == 0 || u_dist > 20) {
+    //     return ir_distance();
+    // }
+    long u_dist = ultrasonic_distance() - ir_distance();
+
     return u_dist;
 }
