e4d9df62c38f6201391d3137601e496ffa31ef66
Duong Nghiep Hoang <134819645+vulturechoujin@users.noreply.github.com>
2025-11-12 00:11:51 +0800

    Add EEPROM

---
+++ Amazing-Race/EEPROM_function.ino
@@ -0,0 +1,30 @@
+void setBlackWhite(){
+  setBalance();
+  int eeAddress = 0;
+  for(int i=0;i<3;++i){
+    EEPROM.put(eeAddress,whiteArray[i]);
+    eeAddress+=sizeof(float);
+  }
+  for(int i=0;i<3;++i){
+    EEPROM.put(eeAddress,blackArray[i]);
+    eeAddress+=sizeof(float);
+  }
+}
+void readBlackWhite(){
+  int eeAddress = 0;
+  for(int i=0;i<3;++i){
+    EEPROM.get(eeAddress,whiteArray[i]);
+    eeAddress+=sizeof(float);
+  } 
+  for(int i=0;i<3;++i){
+    EEPROM.get(eeAddress,blackArray[i]);
+  }
+  eeAddress+=sizeof(float);
+}
+bool checkExist(){
+  int eeAddress = 0;
+  if(whiteArray[eeAddress]!=0){
+     return true;
+  }
+  return false;
+}
