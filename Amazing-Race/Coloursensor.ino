27ae624bb83ccf1c93b77e39cf0ce56210599262
Duong Nghiep Hoang <134819645+vulturechoujin@users.noreply.github.com>
2025-11-11 12:45:40 +0800

    Refactor Coloursensor logic and remove unused code
    Refactor color sensing logic and improve readability.

--- Amazing-Race/Coloursensor.ino
+++ Amazing-Race/Coloursensor.ino
@@ -1,20 +1,9 @@
-#define RGBWait 400
-#define LDRWait 10
-#define LDR 0
-#define PrintGray
-#define PrintBlack
-#define PrintWhite
 MeLineFollower lineFinder(PORT_2);
 int red = 0;
 int green = 0;
 int blue = 0;
-int sentivity = 10;
-char* colours[] = {"Red","Green","Blue","Orange","Pink"};
-float colourArray[] = { 0, 0, 0 };
-float whiteArray[] = { 0, 0, 0 };
-float blackArray[] = { 0, 0, 0 };
-float greyDiff[] = { 0, 0, 0 };
-
+int sentivity = 20;
+int LIMIT = 100;
 void input(int code) {
   if (code == 0) {
     //TURN RED
@@ -35,23 +24,73 @@ void input(int code) {
     digitalWrite(S2, HIGH);
   }
 }
-bool over_half(float color_val) {
-  return (color_val > float(255) / 2);
+
+int getAvgReading(int times) {
+  int reading;
+  int total = 0;
+
+  for (int i = 0; i < times; i++) {
+    reading = analogRead(LDR);
+    total = reading + total;
+    delay(LDRWait);
+  }
+
+  return total / times;
 }
-char* classifyColour(int Red, int Green, int Blue){
-  if(!over_half(Green-sentivity) && !over_half(Blue-sentivity)){
+
+bool detect_black() {
+  int sensorState = lineFinder.readSensors();
+  return (sensorState == S1_IN_S2_IN);
+}
+
+void turnLedOn(char* s){
+  if (strcmp(s,"Red") == 0) led.setColor(255,0,0);
+  else if (strcmp(s,"Green")==0) led.setColor(0,255,0);
+  else if (strcmp(s,"Orange")==0) led.setColor(255,255,0);
+  else if (strcmp(s,"Pink")==0) led.setColor(0,255,255);
+  else if (strcmp(s,"Blue")==0) led.setColor(0,0,255);
+  else if(strcmp(s,"Yellow") == 0) led.setColor(0,255,255);
+  else led.setColor(255,255,255);
+  led.show();
+}
+
+void print_array(float a[]) {
+  for (int i = 0; i < 3; ++i) {
+    Serial.print(a[i]);
+    Serial.print(" ");
+ }
+  Serial.println();
+  delay(500);
+}
+
+void countdown_time(int time) {
+  for (int i = time; i >0; --i) {
+    Serial.print(i);
+    Serial.print(",");
+    delay(500);
+  }
+  Serial.println();
+}
+
+#ifdef Debug_Color
+bool over_limit(float color_val) {
+  return (color_val > LIMIT);
+}
+
+char* classifyColour(float Red, float Green, float Blue){
+  if(!over_limit(Green) && !over_limit(Blue)){
     return "Red";
   }
-  else if(!over_half(Red-sentivity) && !over_half(Green-sentivity)){
+  else if(!over_limit(Red) && over_limit(Blue) && Blue > Green + 1.5*sentivity){
     return "Blue";
   }
-  else if(!over_half(Blue-sentivity) && !over_half(Red-sentivity)){
+  else if(!over_limit(Blue-sentivity) && !over_limit(Red)){
     return "Green";
   }
-  else if(Red+sentivity>Blue && Green+sentivity>Blue){
+  else if(!over_limit(Blue-sentivity)){
     return "Orange";
   }
-  else if(Red>=255-3*sentivity && Green>=255-3*sentivity){
+  else if(Green+1.5 * sentivity>255.0 || Blue + 1.5 *  sentivity > 255.0 || Red + 1.5 * sentivity > 255.0){
     return "White";
   }
   else{
@@ -61,7 +100,10 @@ char* classifyColour(int Red, int Green, int Blue){
 
 
 char* color_sensing() {
-  countdown_time(5);
+  turnLedOn("White");
+  countdown_time(3);
+  led.setColor(0,0,0);
+  led.show();
   for (int c = 0; c <= 2; c++) {
     input(c);
     delay(RGBWait);
@@ -76,28 +118,17 @@ char* color_sensing() {
   char *colour = classifyColour(colourArray[0],colourArray[1],colourArray[2]);
   Serial.print(colour);
   Serial.println("");
+  return colour;
 }
 
-void countdown_time(int time) {
-  for (int i = time; i >= 1; i--) {
-    Serial.print(i + " ");
-    delay(1000);
-  }
-  Serial.println();
-}
 
-void print_array(float a[]) {
-  for (int i = 0; i < 3; ++i) {
-    Serial.print(a[i]);
-    Serial.print(" ");
- }
-  Serial.println();
-  delay(500);
-}
 void setBalance() {
   //set white balance
   Serial.println("Put White Sample For Calibration ...");
-  countdown_time(5);
+  // turnLedOn("White");
+  // countdown_time(5);
+  // led.setColor(0,0,0);
+  // led.show();
   for (int i = 0; i <= 2; i++) {
     input(i);
     delay(RGBWait);
@@ -107,8 +138,12 @@ void setBalance() {
   }
   Serial.println("White Array:");
   print_array(whiteArray);
+  turnLedOn("White");
   Serial.println("Put Black Sample For Calibration ...");
+  turnLedOn("White");
   countdown_time(5);
+  led.setColor(0,0,0);
+  led.show();
   for (int i = 0; i <= 2; i++) {
     input(i);
     delay(RGBWait);
@@ -119,31 +154,5 @@ void setBalance() {
   }
   Serial.println("Black Array:");
   print_array(blackArray);
-  Serial.println("Put colored sheet");
 }
-int getAvgReading(int times) {
-  int reading;
-  int total = 0;
-
-  for (int i = 0; i < times; i++) {
-    reading = analogRead(LDR);
-    total = reading + total;
-    delay(LDRWait);
-  }
-
-  return total / times;
-}
-void turnLedOn(String s){
-  if (s == "Red") led.setColor(255,0,0);
-  else if (s == "Green") led.setColor(0,255,0);
-  else if (s == "Orange") led.setColor(255,255,0);
-  else if (s == "Pink") led.setColor(0,255,255);
-  else if (s == "Blue") led.setColor(0,0,255);
-  else led.setColor(255,255,255);
-  led.show();
-}
-bool detect_black() {
-  int sensorState = lineFinder.readSensors();
-  return (sensorState == S1_IN_S2_IN);
-}
-
+#endif
