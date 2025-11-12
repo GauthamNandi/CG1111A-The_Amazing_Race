af062268af5ae628cbfadc9625765c9d31a3a374
Gautham <124564178+GauthamNandi@users.noreply.github.com>
2025-11-12 09:22:39 +0800

    Adjust motor speed and PID parameters

--- Amazing-Race/MovementFunctions.ino
+++ Amazing-Race/MovementFunctions.ino
@@ -1,5 +1,5 @@
-#define TURN_TIME 625
-#define TURNING_SPEED 180
+#define TURN_TIME 600
+#define TURNING_SPEED 160
 #define MOTOR_SPEED 200
 #define STRAIGHT_TIME 1000
 
@@ -22,7 +22,7 @@ double calculate_PID() {
     double dt = (now - lsttime) / 1000.0;
     if (dt <= 0) dt = 0.001;
 
-    long setpoint = 10;
+    long setpoint = 9;
     double distance = find_distance();
     double error = setpoint - distance;
 
@@ -45,10 +45,17 @@ void stopMotor() {// Code for stopping motor}
     rightMotor.stop();
 }
 
+bool irwawy = false;
+
 void moveForward() {// Code for moving forward for some short interval}
-    speedPID = calculate_PID();
-    leftMotor.run(MOTOR_SPEED - speedPID);
-    rightMotor.run(-MOTOR_SPEED - speedPID);
+    speedPID = -calculate_PID();
+    if (irwawy) {
+        speedPID = +130;
+        irwawy = !irwawy;
+    }
+
+    leftMotor.run(MOTOR_SPEED + speedPID);
+    rightMotor.run(-MOTOR_SPEED + speedPID);
 
      Serial.print("PID: "); Serial.print(speedPID);
     Serial.print(" | L: "); Serial.print(MOTOR_SPEED - speedPID);
@@ -81,7 +88,7 @@ void turnLeft() {
 
 void uTurn() {// Code for u-turn}
     helper_turnLeft();
-    delay(2 * TURN_TIME);
+    delay(1.8 * TURN_TIME);
     stopMotor();
 }
 
