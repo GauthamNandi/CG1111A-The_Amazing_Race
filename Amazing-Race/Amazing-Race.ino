d8e91b4086459f982e33b9544b519e9aa8e9f842
Gautham <124564178+GauthamNandi@users.noreply.github.com>
2025-11-12 09:21:42 +0800

    Refactor debug settings and color handling
    Updated debugging configurations and color sensing logic.

--- Amazing-Race/Amazing-Race.ino
+++ Amazing-Race/Amazing-Race.ino
@@ -1,10 +1,10 @@
 #include "MeOrion.h"
 #define Debug_Color
-#define Debug_Movement
+// #define Debug_Movement
 #define IR_RECIEVER_PIN A1
 #define ULTRASONIC_PIN 12
 
-#define RGBWait 400
+#define RGBWait 200
 #define LDRWait 10
 #define LDR 0
 
@@ -14,9 +14,9 @@ bool FORWARD = true;
 float speedPID;
 char* colours[] = {"Red","Green","Blue","Orange","Pink"};
 float colourArray[] = { 0, 0, 0 };
-float whiteArray[] = { 0, 0, 0 };
-float blackArray[] = { 0, 0, 0 };
-float greyDiff[] = { 0, 0, 0 };
+float whiteArray[] = { 462.0, 378.0, 461.0 };
+float blackArray[] = { 30.0, 27.0, 43.0 };
+float greyDiff[] = { 432.0, 351.0, 418.0 };
 
 // MeBuzzer buzzer;
 MeRGBLed led(PORT_3);
@@ -27,16 +27,30 @@ void setup() {
     pinMode(S2, OUTPUT);
     pinMode(IR_RECIEVER_PIN, INPUT);
     pinMode(ULTRASONIC_PIN, OUTPUT);
-    #ifdef Debug_Color
-    setBalance();
-    #endif 
+    // #ifdef Debug_Color
+    // setBalance();
+    // #endif 
     // delay(10000);
 }
 
 void loop() {
+    char* s = color_sensing();
+    Serial.println(s);
+    // turnLedOn(s);
+    // delay(1000);
+    // led.setColor(0,0,0);
+    // led.show();
+    // Serial.println(read_ir());
+    // Serial.println(ultrasonic_distance());
+    // moveForward();
+    // doubleLeftTurn(); delay(1000);
+    // doubleRightTurn(); delay(1000);
+    // turnLeft(); delay(1000);
+    // Serial.println(detect_black());
+    // moveForward();
     #ifdef Debug_Movement
     if (FORWARD) {
-        speedPID = calculate_PID();
+        // speedPID = calculate_PID();
         moveForward();
         
         if (detect_black()) {
@@ -49,16 +63,16 @@ void loop() {
         char* s = color_sensing();
         Serial.println(s);
         turnLedOn(s);
-        delay(1000);
+        delay(100);
         led.setColor(0,0,0);
         led.show();
 
         if (strcmp(s,"Red") == 0) turnLeft();
         else if (strcmp(s,"Green") == 0) turnRight();
         else if (strcmp(s,"Orange") == 0) uTurn();
-        else if (strcmp(s,"Pink")) doubleLeftTurn();
-        else if (strcmp(s,"Blue")) doubleRightTurn();
-        else if (strcmp(s,"White")) celebrate();
+        else if (strcmp(s,"Pink") == 0) doubleLeftTurn();
+        else if (strcmp(s,"Blue") == 0) doubleRightTurn();
+        else if (strcmp(s,"White") == 0) celebrate();
         moveForward();
         delay(100);
         FORWARD = true;
